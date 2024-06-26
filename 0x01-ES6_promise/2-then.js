544a05cbd222b7d381e2ebc720908edf72408048
asmaagoma <asmaagadallaah@gmail.com>
2024-06-27 01:31:04 +0300

    :"(

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,15 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
+    .then(() => {
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      throw new Error('Error occurred');
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }

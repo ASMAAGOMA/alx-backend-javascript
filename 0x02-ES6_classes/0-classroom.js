ba4d5e979d29dbfa89f9be4a32bed48d39d8b313
asmaagoma <asmaagadallaah@gmail.com>
2024-06-29 00:30:44 +0300

    :"(

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,12 @@
+/**
+ * classroom
+ */
 export default class ClassRoom{
+  /**
+  * classroom
+  * @param {Number} _maxStudentsSize - num of students 
+  */
   constructor(_maxStudentsSize){
     this._maxStudentsSize = _maxStudentsSize;
   }
-};
+}

f8b51ed713705bc27075afd33377d71dfab59c36
asmaagoma <asmaagadallaah@gmail.com>
2024-06-29 01:11:21 +0300

    :"(

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,6 +2,6 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   let classRooms;
-  classRooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  classRooms = [ new ClassRoom(19), new ClassRoom(20), new ClassRoom(34) ];
   return classRooms;
 }

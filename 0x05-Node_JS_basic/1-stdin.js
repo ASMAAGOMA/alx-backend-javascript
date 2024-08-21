414a562b049881f4c5400cc9fd31e3ce80690c98
asmaagoma <asmaagadallaah@gmail.com>
2024-08-21 18:15:24 +0300

    :"(

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,9 +2,10 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const inp = process.stdin.read();
   if (inp) {
-    console.log(`Your name is: ${inp}`);
+    const name = inp.toString().trim();
+    console.log(`Your name is: ${name}`);
   }
 });
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });

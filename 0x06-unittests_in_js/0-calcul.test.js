a843493fc71c58dd41bc71035e744c810e97af1f
asmaagoma <asmaagadallaah@gmail.com>
2024-08-22 18:10:55 +0300

    :"(

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,22 @@
-//import assert from 'assert';
-
 const assert = require('assert');
-
-//import calculateNumber from './0-calcul.js';
-const calculateNumber = require('./0-calcul')
+var calculateNumber = require('./0-calcul');
 
 
 describe('calculateNumber', () => {
   it('should return 4 when inputs are 1 and 3', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
+
   it('should return 5 when inputs are 1 and 3.7', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
+
   it('should return 5 when inputs are 1.2 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
+
   it('should return 6 when inputs are 1.5 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+
 });

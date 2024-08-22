bcab6e492bd07504783f65a525f9ab0bc62a74a5
asmaagoma <asmaagadallaah@gmail.com>
2024-08-22 17:46:23 +0300

    :"(

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+import assert from 'assert';
+import calculateNumber from './0-calcul.js';
+
+describe('calculateNumber', () => {
+  it('should return 4 when inputs are 1 and 3', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return 5 when inputs are 1 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should return 5 when inputs are 1.2 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return 6 when inputs are 1.5 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
